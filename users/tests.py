de9942b7e2db3453eb25b362e0d01e35e4c10a46
Tushar57517 <tushar.saini1210@gmail.com>
2026-01-13 21:40:26 +0530

    add register functionality in users app

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+User = get_user_model()
 
 # Create your tests here.
+class RegisterViewTest(TestCase):
+    
+    def test_register_template_loads(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_user_registration_success(self):
+        response = self.client.post(reverse("register"), {
+            "username": "testuser",
+            "password1": "StrongPass123!",
+            "password2": "StrongPass123!",
+        })
+
+        self.assertEqual(User.objects.count(), 1)
+        self.assertTrue(User.objects.filter(username="testuser").exists())
+
+        self.assertEqual(response.status_code, 302)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegisterForm
 
 # Create your views here.
+def registerView(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form':form})
