39d6d269ac01f24542e6846f11336eb115541d6f
Tushar57517 <tushar.saini1210@gmail.com>
2026-01-14 19:51:45 +0530

    update dashboard template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from pages.models import Page
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 # Create your views here.
+@login_required
 def dashboard_view(request):
-    pages = Page.objects.all()
-    return render(request, 'dashboard/dashboard.html', {'pages':pages})
+    current_hr = datetime.now().hour
+    if 5 <= current_hr < 12:
+        time_of_day = "morning"
+    elif 12 <= current_hr < 17:
+        time_of_day = "afternoon"
+    else:
+        time_of_day = "evening"
+    pages = Page.objects.filter(owner=request.user)
+    return render(request, 'dashboard/dashboard.html', {'pages':pages, 'time_of_day':time_of_day})
